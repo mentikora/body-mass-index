90a9de644f666a11673bc819ba334ad6f88a64e7
horo <notender_boy@ukr.net>
2016-12-22 18:03:35 +0200

    update, history

--- dist/main_jquery.js
+++ dist/main_jquery.js
@@ -2,24 +2,29 @@
 
 $.noConflict();
 jQuery(document).ready(function($){
-
+  
+  let history = {}, 
+      clickCounter = 0;
+  
   $('#check_btn').on('click', function(){
 
     // variables
     let userHeight = $('#user_height').val(),
-        userWeight = $('#user_weigth').val(),
-        tableTr = $('.table tbody tr'),
+        userWeight = $('#user_weight').val(),
+        tableTr = $('.bmi_result_table tbody tr'),
         bmiResult = userWeight / Math.pow(userHeight / 100, 2),
         getTrCounter;
 
     // check height&weight
     if( (userHeight < 130) || (userHeight > 250) ){
       alert('Height is not valid');
-      $('.table').slideUp();
+      $('.hidden_block').slideUp();
+      return
     }
     if( (userWeight < 30) || (userWeight > 300) ){
       alert('Weight is not valid');
-      $('.table').slideUp();
+      $('.hidden_block').slideUp();
+      return
     }
 
     // table magic
@@ -46,10 +51,28 @@ jQuery(document).ready(function($){
     tableTr.removeClass('highlight');
     $(tableTr[getTrCounter]).addClass('highlight'); // ???? ask Antonio about this
 
+    /*
+      trying to make history
+      should looks like:
+      { clickCounter:{height:.., weight:.., bmi:...}, ... }
+    */
+    history[clickCounter] = {
+      height: userHeight,
+      weight: userWeight,
+      bmi: bmiResult.toFixed(2)
+    }
+
+    for(var i = clickCounter; i < Object.keys(history).length; i++){
+      $('.history').append('<li>'+ 'Height: '+ history[i].height + ', weight: ' + history[i].weight + ', bmi: ' + history[i].bmi + '</li>');
+    }
+
+    clickCounter++;
+
     // show hidden block with result and table
     $('.user_bmi_result').html( bmiResult.toFixed(2) );
     $('.hidden_block').slideDown();
 
+    console.log(history);
   });
 
 });
